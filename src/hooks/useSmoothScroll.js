b9deb5c9a4df6793e09b05c6e749f3d7ddbd6574
Siavash Afsari <siwash.44@gmail.com>
2026-08-05 20:05:25 +0330

    Reduce scroll rate and increase Lenis smooth scroll inertia

--- src/hooks/useSmoothScroll.js
+++ src/hooks/useSmoothScroll.js
@@ -1,12 +1,17 @@
 import { useEffect } from 'react';
-import Lenis from 'lenis';
+import Lenis from '@studio-freight/lenis';
 
 export default function useSmoothScroll() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      duration: 1.8,          // Higher duration = slower, smoother scroll stop (Default is ~1.2)
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Smooth exponential ease-out
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
       smoothWheel: true,
+      wheelMultiplier: 0.7,   // Lower multiplier = slower scroll movement per wheel notch (Default is 1.0)
+      touchMultiplier: 1.2,   // Slower response for touchpads/mobile
+      infinite: false,
     });
 
     function raf(time) {

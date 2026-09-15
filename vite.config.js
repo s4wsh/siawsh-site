a06063d6af34b1896102d80ed0443605ca2aed77
Siavash Afsari <siwash.44@gmail.com>
2026-09-15 03:53:13 +0330

    fix(build): generate robots.txt via vite-plugin-sitemap for siavashstudio.ir

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import sitemap from 'vite-plugin-sitemap'
 import { projectsData } from './src/data/projectsData.js'
 
-const HOSTNAME = 'https://www.siawsh.co'
+const HOSTNAME = 'https://siavashstudio.ir'
 const dynamicProjectRoutes = projectsData.map((project) => `/work/${project.id}`)
 
 export default defineConfig({
